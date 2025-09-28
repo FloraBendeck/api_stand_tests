7ed578e70251509dcedaccde1f0c912245cba83e
Flora Bendeck <comodinneutral@gmail.com>
2025-09-28 12:10:21 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE =" https://cnt-606af7e3-2221-4941-b5fb-a6bc6abed411.containerhub.tripleten-services.com"
+#almacena la URL
+
+#diferentes endpoints
+DOC_PATH = "/docs/" #ruta a documentación
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"#ruta a tabla usuarios
+CREATE_USER_PATH = "/api/v1/users/"#ruta para crear tablas
+
+#Metodo+URL+endpoint+parametros
---
+++ data.py
@@ -0,0 +1,9 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "lucia",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
